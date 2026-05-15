02f9fff14f5aee5eb34d89f311b11864a0879ea5
vince-ing <vince-ing@utexas.edu>
2026-05-14 23:47:19 -0500

    lambda print

--- arctoq_plugin.py
+++ arctoq_plugin.py
@@ -270,6 +270,7 @@ class ConvertDialog(QDialog):
         self._worker = BatchWorker(files, out_dir, save_in_place, mirror_structure, allow_overwrite, common_base)
         self._worker.progress.connect(self.progress_bar.setValue)
         self._worker.file_result.connect(self._on_file_result)
+        self._worker.finished.connect(lambda: print("LAMBDA FIRED"), Qt.QueuedConnection)
         self._worker.finished.connect(self._on_batch_finished, Qt.QueuedConnection)
         print("finished signal connected")
         self._worker.start()
